1fefa0841bfd9222d4fe21efb2dcbed357a6a9af
paperworkmonkey <161349050+paperworkmonkey@users.noreply.github.com>
2026-01-22 18:50:56 +0000

    Updated formulae. Still needs gamblegrams.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   AcidBase.css
    	modified:   AcidBase.html
    	modified:   AcidBase.js

--- AcidBase.js
+++ AcidBase.js
@@ -1,14 +1,20 @@
-let pHValue;
-let PCO2Value;
-let HCO3Value;
-let NaValue;
-let KValue;
-let ClValue;
-let CaTotValue;
-let MgValue;
-let AlbuminValue;
-let PhosphateValue;
-let LactateValue;
+let pH;
+let PCO2;
+let HCO3;
+let Na;
+let K;
+let Cl;
+let CaTot;
+let Mg;
+let Albumin;
+let Phosphate;
+let Lactate;
+let Hb;
+let MeasuredOsm;
+let Urea;
+let Glucose;
+let sBEHb50;
+let sBEHbPt;
 let debugging = false;
 
 function setup() {
@@ -23,8 +29,12 @@ function setup() {
   // AlbuminValue = document.getElementById("AlbuminInput");
   // PhosphateValue = document.getElementById("PhosphateInput");
   // LactateValue = document.getElementById("LactateInput");
+  // MeasuredOsm = document.getElementById("MeasuredOsmInput");
+  // UreaValue = document.getElementById("UreaInput");
+  // GlucoseValue = document.getElementById("GlucoseInput");
 
   noCanvas();
+  updateResult();
 }
 
 function updateResult() {
@@ -41,6 +51,10 @@ function updateResult() {
   let AlbuminValue = document.getElementById("AlbuminValue");
   let PhosphateValue = document.getElementById("PhosphateValue");
   let LactateValue = document.getElementById("LactateValue");
+  let HbValue = document.getElementById("HbValue");
+  let MeasuredOsmValue = document.getElementById("MeasOsmValue");
+  let UreaValue = document.getElementById("UreaValue");
+  let GlucoseValue = document.getElementById("GlucoseValue");
 
   if (debugging) {
     // console.log("pHValue element:", pHValue.value);
@@ -54,6 +68,10 @@ function updateResult() {
     // console.log("AlbuminValue element:", AlbuminValue.value);
     // console.log("PhosphateValue element:", PhosphateValue.value);
     // console.log("LactateValue element:", LactateValue.value);
+    // console.log("HbValue element:", HbValue.value);
+    // console.log("MeasuredOsmValue element:", MeasuredOsmValue.value);
+    // console.log("UreaValue element:", UreaValue.value);
+    // console.log("GlucoseValue element:", GlucoseValue.value);
   }
 
   // Check if any of the elements are null before trying to access their 'value' property
@@ -68,24 +86,32 @@ function updateResult() {
     !MgValue ||
     !AlbuminValue ||
     !PhosphateValue ||
-    !LactateValue
+    !LactateValue ||
+    !HbValue ||
+    !MeasuredOsmValue ||
+    !UreaValue ||
+    !GlucoseValue
   ) {
     console.error("One or more required input elements not found in the DOM.");
     // Optionally, you might want to return or set a default value for myResult
     return;
   }
 
-  let pH = parseFloat(pHValue.value || 0);
-  let PCO2 = parseFloat(PCO2Value.value || 0);
-  let HCO3 = parseFloat(HCO3Value.value || 0);
-  let Na = parseFloat(NaValue.value || 0);
-  let K = parseFloat(KValue.value || 0);
-  let Cl = parseFloat(ClValue.value || 0);
-  let CaTot = parseFloat(CaTotValue.value || 0);
-  let Mg = parseFloat(MgValue.value || 0);
-  let Phosphate = parseFloat(PhosphateValue.value || 0);
-  let Lactate = parseFloat(LactateValue.value || 0);
-  let albumin = parseFloat(AlbuminValue.value || 0);
+  pH = parseFloat(pHValue.value || 0);
+  PCO2 = parseFloat(PCO2Value.value || 0);
+  HCO3 = parseFloat(HCO3Value.value || 0);
+  Na = parseFloat(NaValue.value || 0);
+  K = parseFloat(KValue.value || 0);
+  Cl = parseFloat(ClValue.value || 0);
+  CaTot = parseFloat(CaTotValue.value || 0);
+  Mg = parseFloat(MgValue.value || 0);
+  Phosphate = parseFloat(PhosphateValue.value || 0);
+  Lactate = parseFloat(LactateValue.value || 0);
+  albumin = parseFloat(AlbuminValue.value || 0);
+  Hb = parseFloat(HbValue.value || 0);
+  MeasuredOsm = parseFloat(MeasuredOsmValue.value || 0);
+  Ur = parseFloat(UreaValue.value || 0);
+  Gluc = parseFloat(GlucoseValue.value || 0);
 
   let AnionGap = Na + K - (Cl + HCO3);
   let CorrAnionGap = Na - (Cl + HCO3) + 0.25 * (albumin - 40);
@@ -107,6 +133,16 @@ function updateResult() {
   // CO2  as bicarb =0.23 * pCO2 * 10^(pH - 6.1)
   //phosphate (B12*(0.309*B9-0.469))
 
+  sBEHb50 =
+    (CO2asBicarb - 24.4 + (2.3 * (50 / 10) + 7.7) * (pH - 7.4)) *
+    (1 - (0.023 * 50) / 10);
+  sBEHbPt =
+    (CO2asBicarb - 24.4 + (2.3 * (Hb / 10) + 7.7) * (pH - 7.4)) *
+    (1 - (0.023 * Hb) / 10);
+
+  let OsmCalc = 2 * (Na + K) + Ur + Gluc;
+  let OsmGap = MeasuredOsm - OsmCalc;
+
   document.getElementById("AnionGapBox").innerText =
     Number(AnionGap).toFixed(1);
   document.getElementById("CorrAnionGapBox").innerText =
@@ -124,8 +160,12 @@ function updateResult() {
     Number(PhosphateEffect).toFixed(1);
   document.getElementById("CO2asBicarbBox").innerText =
     Number(CO2asBicarb).toFixed(1);
+  document.getElementById("sBEHb50Box").innerText = Number(sBEHb50).toFixed(1);
+  document.getElementById("sBEHbPtBox").innerText = Number(sBEHbPt).toFixed(1);
+  document.getElementById("OsmGapBox").innerText = Number(OsmGap).toFixed(1);
 
   drawGamblegram();
+  updateInterpretation();
 }
 
 function drawGamblegram() {
@@ -145,4 +185,31 @@ function drawGamblegram() {
   //draw Albumin
   //draw Lactate
   //draw Phosphate
+}
+
+function updateInterpretation() {
+  //console.log("Updating Interpretation...");
+  console.log(pH);
+  let aemia;
+  if (pH < 7.35) {
+    aemia = "acidaemia";
+    if (PCO2 < 4.0) {
+      aemia += " with respiratory compensation";
+    } else if (PCO2 > 6.0) {
+      aemia += " with respiratory acidosis";
+    }
+  } else if (pH > 7.45) {
+    aemia = "alkalaemia";
+    if (sBEHbPt < -2) {
+      aemia += " with metabolic compensation";
+    } else if (sBEHbPt > 2) {
+      aemia += " with metabolic alkalosis";
+    }
+  } else {
+    aemia = "normal pH";
+  }
+
+  let interpretationText = `The patient has ${aemia}. `;
+
+  document.getElementById("interpretationBox").innerText = interpretationText;
 }
